2a26a8a64f75dcdc195168a14f30c0d9676ab6a7
Abinaya Srikant <abinaya.srikant@berkeley.edu>
2020-07-14 00:24:41 -0700

    Top of About page

--- src/containers/About.js
+++ src/containers/About.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import NavBar from '../components/NavBar';
 import '../css/About.scss';
+import aboutPhoto from '../img/about/about-header.png';
 
 class About extends React.Component {
   render() {
     document.title = 'About NIB';
-    
-    
     return (
       <section class="aboutPage">
         <NavBar />
-        <section class="container">
-            <p>About Page</p>
+        <section class="hero container">
+          <div id="left">
+            <h3>ABOUT US</h3>
+            <h1>Inspired by our mission</h1>
+            <p>Through our focus on social and environmental impact,
+            innovative methods, and diverse backgrounds, we strive to
+            bring social responsibility and environmental stewardship
+            to business and innovation.  </p>
+          </div>
+          <div id="right">
+            <img src= {aboutPhoto} />
+          </div>
         </section>
       </section>
     );
