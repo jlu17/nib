39fd5f40c9a8229bae38b871f34fde7587d52a39
Adhiv Dhar <adhivd@gmail.com>
2020-07-21 22:13:56 -0700

    blob images

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -13,6 +13,9 @@ import larkin from '../img/home/LarkinStreet.png';
 import marin from '../img/home/MarinFoodBank.png';
 import sierra from '../img/home/SierraNevada.png';
 
+import blob1 from '../img/home/shapes/blob1.png';
+import blob2 from '../img/home/shapes/blob2.png';
+
 
 class Home extends React.Component {
   render() {
@@ -32,7 +35,17 @@ class Home extends React.Component {
                 />
             </span>
             <span id="right">
-                <p>images and stuff</p>
+                <svg className="bigBlob" width="900" height="791" viewBox="0 0 900 791" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M417.558 178.016C450.216 205.519 522.863 215.23 535.287 235.635C547.89 255.699 499.431 286.453 483.259 340.604C466.578 394.923 481.827 473.321 458.046 472.195C434.265 471.069 373.005 390.59 321.637 376.35C270.599 362.282 228.945 414.622 187.183 430.183C145.42 445.743 101.462 425.377 61.6295 391.516C21.9759 357.314 -13.5523 309.617 6.64554 271.048C26.3347 232.647 102.259 203.206 137.358 160.252C172.306 116.784 166.099 59.6326 186.53 29.4451C206.631 -0.914927 253.698 -4.1384 293.794 5.94963C333.891 16.0377 367.525 39.2683 380.195 72.8443C393.195 106.593 385.051 151.028 417.558 178.016Z" fill="#79ECFF"/>
+                </svg>
+
+                <svg className="miniBlob" width="91" height="75" viewBox="0 0 91 75" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M89.6851 32.0176C93.2985 41.7664 81.203 57.1611 65.3926 66.226C49.4787 75.321 29.9208 77.9597 17.5121 70.9377C5.10338 63.9157 0.0150779 47.236 0.413986 32.1847C0.916331 17.1033 6.9738 3.68343 15.7691 0.956559C24.5289 -1.70698 35.9586 6.28935 51.3346 12.3391C66.7462 18.3254 86.0361 22.3321 89.6851 32.0176Z" fill="#79ECFF"/>
+                </svg>
+
+                <img src={blob1} class="seniors" />
+                <img src={blob2} class="emgajen" />
+
             </span>
         </section>
 
