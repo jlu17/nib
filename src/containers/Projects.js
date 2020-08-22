98d148243cc6ba9581957381b620023f99baf8d1
Jennifer Lu <jennifer.z.lu@gmail.com>
2020-08-21 17:38:40 -0700

    edits to project page: copy and removed mobile spectrum

--- src/containers/Projects.js
+++ src/containers/Projects.js
@@ -37,11 +37,10 @@ class Projects extends React.Component {
             <section className="hero container">
                 <div className="left">
                     <h3>PROJECTS</h3>
-                    <h1>We Make Our Impact Through Dedicated Client Work</h1>
-                    <p>Each semester we work alongside companies, non-profits and start-ups on a socially/environmentally driven problem. <br />
+                    <h1>Making an Impact <br /> With Our Clients</h1>
+                    <p>Each semester, our teams help companies, social enterprises, and non-profits form strategies to solve social and environmental problems. <br />
                     <br />
-                    Defining the work space, conducting client research, and analyzing data are just a part of what we do in order to take a
-                    complex problem and compact it into an understandable and viable solution that will make a difference.  </p>
+                    We take a multi-disciplinary approach to working on projects across a wide range of industries and impact areas.</p>
                 </div>
                 <div className="right heroImage verticallyCenter">
                     <img src={ProjectHeroImage} alt="NIB members collage" />
@@ -52,50 +51,25 @@ class Projects extends React.Component {
             </svg>
             <section className="projectTypesSection">
                 <section className="container">
-                    <h1>What Kind of Work We Do</h1>
-                    <p>The sky’s the limit. But just as an example of some of the work we’ve done in the past few semesters...</p>
+                    <h1>Our Solutions</h1>
+                    <p>For over a decade, we’ve developed and expanded our consulting capabilities to support clients in many ways, some of which include:</p>
                     <div className="flex projectTypes">
                         {projectTypes}
                     </div>
                 </section>
             </section>
             <section className="container projectSpectrumSection">
-                <h1>Who Our Clients Are</h1>
-                <p>You can imagine it as a spectrum. Whether it's large for-profit corporations that allow us to deep dive into
+                <h1>Our Clients</h1>
+                <img className="hideOnMobile" src={ClientSpectrumNew} alt="Spectrum of projects ranging from small nonprofits to large corporations" />
+                <p>We work with organizations of all kinds that are interested in contributing to a better, more sustainable world. 
+                Whether it's for-profit corporations that allow us to deep dive into
                 one component of their programs, mission-driven corporations that continuously strive for solutions beyond the
                 baseline of "good enough", or smaller social enterprises and nonprofits that give us the opportunity to touch
-                multiple aspects, we work with them all. As long as our work for our clients is aiming to create a better, more sustainable world. </p>
-                <img className="hideOnMobile" src={ClientSpectrumNew} alt="Spectrum of projects ranging from small nonprofits to large corporations" />
-                <div className="mobileTimeline showOnMobile">
-                    <h3>Smaller Social Enterprises and Nonprofits</h3>
-                    <div className="grid">
-                        <img src={require("../img/projects/pastClients/larkin.png")} alt="Larkin Youth Street Services" />
-                        <img src={require("../img/projects/pastClients/volunteermatch.png")} alt="VolunteerMatch" />
-                        <img src={require("../img/projects/pastClients/marinfoodbank.png")} alt="SF-Marin Food Bank" />
-                        <img src={require("../img/projects/pastClients/stopthetraffik.png")} alt="Stop the Traffik" />
-                    </div>
-                    <h3>Mission-Driven Corporations</h3>
-                    <div className="grid">
-                        <img src={require("../img/projects/pastClients/lo3energy.png")} alt="Larkin Youth Street Services" />
-                        <img src={require("../img/projects/pastClients/kulikuli.png")} alt="VolunteerMatch" />
-                        <img src={require("../img/projects/pastClients/imperfectfoods.png")} alt="SF-Marin Food Bank" />
-                        <img src={require("../img/projects/pastClients/semtive.png")} alt="Stop the Traffik" />
-                    </div>
-                    <h3>Large For-Profit Corporations</h3>
-                    <div className="grid">
-                        <img src={require("../img/projects/pastClients/casper.png")} alt="Larkin Youth Street Services" />
-                        <img src={require("../img/projects/pastClients/stripe.png")} alt="VolunteerMatch" />
-                        <img src={require("../img/projects/pastClients/lyft.png")} alt="SF-Marin Food Bank" />
-                        <img src={require("../img/projects/pastClients/godaddy.png")} alt="Stop the Traffik" />
-                    </div>
-                </div>
-            </section>
-            <section className="container clientsSection">
-                <h1>Past Clients</h1>
-                <div class="hideOnMobile">
+                multiple aspects, we work with them all. </p>
+                <div class="clientsSection hideOnMobile">
                     {clientImages}
                 </div>
-                <div class="showOnMobile">
+                <div class="clientsSection showOnMobile">
                     {clientImagesMobile}
                 </div>
             </section>
--- src/data/ProjectTypesText.js
+++ src/data/ProjectTypesText.js
@@ -1,27 +1,27 @@
 let projectTypeInfo = [
     {
         img: require('../img/projects/projectTypes/product.png'),
-        alt: "Figma logo",
-        title: "Designing a New Product",
-        description: "Is there a need for this product? Why doesn't/does this project already exist and what additional features should be incorporated? How should this product look? These are the questions we answer."
+        alt: "Product design logo",
+        title: "New Product Development",
+        description: "Investigating product need, design thinking & prototyping, conceptualizing key features, preparing go-to-market strategies"
     },
     {
         img: require('../img/projects/projectTypes/environment.png'),
         alt: "Cycle",
-        title: "Environmental Analysis",
-        description: "Looking at the supply chain of current practices, whether that is a lifecycle analysis or taking a deep dive into specific business operations, and improving our client's respective carbon footprint."
+        title: "Environmental Strategy",
+        description: "Sustainability target planning, life cycle assessment, supply chain greening, carbon neutrality tactics, footprint & resource use reduction"
     },
     {
         img: require('../img/projects/projectTypes/analysis.png'),
         alt: "Excel logo",
-        title: "Customer and Market Research",
-        description: "Customers say they want X when really what they need is Y. This Y is what we aim to discover by providing a robust go to market landscape that includes analyzing user data, conducting SWOT analysis, and providing investment recommendations."
+        title: "Target Market Research",
+        description: "Understanding customer needs, classifying user profiles, competitive landscape overviews, market data analysis, opportunity sizing & positioning"
     },
     {
         img: require('../img/projects/projectTypes/impact.png'),
         alt: "Money bags",
         title: "Impact Evaluation",
-        description: "Once a program or idea is implemented, what are the actual outcomes? This includes defining the right metrics, identifying and analyzing the impact, and weighing the qualitative and quantitative results."
+        description: "Codifying metrics, measurement tactics, stakeholder engagement & communication, quantitative and qualitative analysis of outcomes"
     }
 ];
 
