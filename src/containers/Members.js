aa1dfac18ca771bcbc7e05b691a7c2b255ae98b3
Jennifer Lu <jennifer.z.lu@gmail.com>
2020-08-22 18:18:37 -0700

    members edit

--- src/containers/Members.js
+++ src/containers/Members.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import imageLeft from '../img/images-left.png';
-import imageRight from '../img/images-right.png';
+import imageLeft from '../img/members/splash-left.png';
+import imageRight from '../img/members/splash-right.png';
 import AlumniCompanies from '../data/AlumniCompanies';
 import Member from './../components/Member.js';
 import '../css/Members.scss';
@@ -72,28 +72,22 @@ class Members extends React.Component {
                 />
         });
 
-        let responsive =   {
-            0: {
-                items: 1,
-            },
-            1024: {
-                items: 2
-            }
-        };
-
         return (
         <section className="membersPage">
-            <section>
-                <section className="container membersHero">
-                    <img className="hideOnMobile" src={imageLeft} alt="Collage of individual NIB members on Zoom" />
+            <section className="membersHero">
+                <section className="container">
+                    <img src={imageLeft} alt="Collage of individual NIB members on Zoom" />
                     <h1>Meet Us</h1>
                     <img src={imageRight} alt="Collage of individual NIB members on Zoom" />
                 </section>
             </section>
-            <svg width="600" className="splashWaveLeft" viewBox="0 0 600 569" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <svg className="invertedWave showOnMobile" viewBox="0 0 1440 150" fill="#F0FCFE" xmlns="http://www.w3.org/2000/svg">
+                <path d="M-24 101.603L57.6667 81.0547C139.333 60.1201 302.667 0.789418 466 0.5C629.333 0.789418 792.667 90.9914 956 101.603C1119.33 112.215 1282.67 60.1201 1364.33 50.1834L1446 39.8609V194.217H1364.33C1282.67 194.217 1119.33 194.217 956 194.217C792.667 194.217 629.333 194.217 466 194.217C302.667 194.217 139.333 194.217 57.6667 194.217H-24V101.603Z" fill="#ffffff"/>
+            </svg>
+            <svg width="600" className="splashWaveLeft hideOnMobile" viewBox="0 0 600 569" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M229.5 22.5C170.5 2 0 0 0 0V562.5C0 562.5 99 575.004 132.5 562.5C166 549.996 186 520 258.5 483C331 446 407 521.5 493.5 515.5C580 509.5 638.5 451 567.5 362C496.5 273 302.5 323.5 272.5 260.5C242.5 197.5 334.5 207.5 338 143C341.5 78.5 288.5 43 229.5 22.5Z" fill="#F0FCFE"/>
             </svg>
-            <svg width="599" className="splashWaveRight" viewBox="0 0 599 569" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <svg width="599" className="splashWaveRight hideOnMobile" viewBox="0 0 599 569" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M369.721 545.557C428.721 566.057 599.221 568.057 599.221 568.057V5.55713C599.221 5.55713 500.221 -6.94641 466.721 5.55713C433.221 18.0607 413.221 48.0571 340.721 85.0571C268.221 122.057 192.221 46.5571 105.721 52.5571C19.2207 58.5571 -39.2793 117.057 31.7207 206.057C102.721 295.057 296.721 244.557 326.721 307.557C356.721 370.557 264.721 360.557 261.221 425.057C257.721 489.557 310.721 525.057 369.721 545.557Z" fill="#F0FCFE"/>
             </svg>
             <section>
@@ -109,7 +103,6 @@ class Members extends React.Component {
             </svg>
             <section className="carousel hideOnMobile">
                 <section className="container">
-
                     <div onClick={() => this.Carousel.slidePrev()} className="carouselPointer">
                         <svg width="30" viewBox="0 0 65 65" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <rect x="40.7681" y="56.2985" width="11.897" height="45.7575" transform="rotate(135 40.7681 56.2985)" fill="#1B67B1"/>
@@ -147,12 +140,12 @@ class Members extends React.Component {
                     <div className="verticallyCenter">
                         <h2>Alumni</h2>
                         <p>After their time in Net Impact Berkeley, our alumni have gone on to make their impacts in a wide 
-                        array of career paths from consulting to software engineering to finance to grad school and more.</p>
+                        array of career paths, from consulting to software engineering to finance to grad school and more.</p>
                     </div>
                 </section>
                 <section className="container">
                     <h2>Where Our Alumni Are</h2>
-                    <div id = "company-logos">
+                    <div className="company-logos">
                         {AlumniCompanies.map((company, i) =>
                             <AlumniCompany key={i} name={company.name} href={company.href} fileName={company.fileName} />)}
                     </div>
