6f47308c3cbf50fe10bf4934970021c47243b8c2
Jennifer Lu <jennifer.lu@berkeley.edu>
2020-08-26 21:53:46 -0700

    more merge conflicts

--- src/components/CallToAction.js
+++ src/components/CallToAction.js
@@ -3,11 +3,10 @@ import Button from './Button';
 import '../css/CallToAction.scss';
 
 function CallToAction() {
-    const viewBox = [0, 0, 1440, 239];
     return (
         <div>
           <div className="topsvg">
-            <svg viewBox={viewBox} fill="none" xmlns="http://www.w3.org/2000/svg" className="wave">
+            <svg viewBox="0 0 1440 239" fill="none" xmlns="http://www.w3.org/2000/svg" className="wave">
                 <path d="M692.426 116.5C486.926 116.5 -2.07446 238.5 -2.07446 238.5H1440.93V0C1440.93 0 1408.43 183.5 1193.93 184.5C979.426 185.5 897.926 116.5 692.426 116.5Z" fill="#F0FCFE"/>
             </svg>
             </div>
@@ -15,8 +14,11 @@ function CallToAction() {
                 <h1>Ready to Join Us?</h1>
                 <Button
                     link="/apply"
-                    buttonText="Apply Now"
+                    buttonText="Learn How to Apply"
                 />
+                <div className="application">
+                    <a href="https://airtable.com/shrhW4PzvxhFM6zEi" target="_blank">Application Link →</a>
+                </div>
             </div>
         </div>
     );
